72a00833c7ac41a023a36086a713ba98782322e2
rickganesh <rickganesh@gmail.com>
2020-06-18 23:44:52 -0500

    Initial commit

---
+++ app/src/main/java/com/rithvik/aysi/MainActivity.java
@@ -0,0 +1,27 @@
+package com.rithvik.aysi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "AYSI is cool!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
